96b7b61162365c6ba2f6facdb864b9832a3f2fe5
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-24 13:48:04 +0530

    added the page heaing to home.js

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Slider from '../../components/slider/Slider';
 import HomeInfoBox from './HomeInfoBox';
-import "./Home.scss"
+import "./Home.scss";
+
+
+const PageHeading = ({heading, btnText}) => {
+  return (
+    <>
+      <div className='--flex-between'>
+        <h2 className='--fw-thin'>{heading}</h2>
+        <button className='--btn'>
+          {btnText}
+        </button>
+      </div>
+      <div className='--hr'></div>
+    </>
+  )
+}
 const Home = () => {
   return (
     <>
       <Slider />
-
       <section>
         <div className="container">
           <HomeInfoBox />
+          <PageHeading heading="Latest Product" btnText="Shop Now"/>
         </div>
       </section>
     </>
