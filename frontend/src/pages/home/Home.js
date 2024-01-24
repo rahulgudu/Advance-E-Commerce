9a099b011f07a4e1c65b0b58f7f1b8db3963f81b
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-24 13:35:25 +0530

    added the home info box container

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Slider from '../../components/slider/Slider';
-
+import HomeInfoBox from './HomeInfoBox';
+import "./Home.scss"
 const Home = () => {
   return (
     <>
       <Slider />
+
+      <section>
+        <div className="container">
+          <HomeInfoBox />
+        </div>
+      </section>
     </>
   )
 }
