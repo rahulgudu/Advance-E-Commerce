9bc55392d92a464395e7f20c2d35c76f56721785
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-28 01:55:39 +0530

    added the basic code for backend with mongo conn

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello World!!!");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = process.env.PORT || 6969;
+
+
+// Routes 
+app.get("/", (req, res)=>{
+    res.send("Home Page");
+});
+
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on http://localhost:${PORT}`);
+    })
+}).catch((err)=>console.log(err));
